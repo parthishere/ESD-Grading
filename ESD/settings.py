c8f10519c59e18ea98cb22f6d32daf013ec83808
parthishere <parthishere1234@gmail.com>
2025-03-18 07:13:08 -0600

    logout working

--- ESD/settings.py
+++ ESD/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 print("DEBUG vars ->")
 
-DEBUG = False
+DEBUG = True
 # DEBUG = False
 
 ALLOWED_HOSTS = ['*']
